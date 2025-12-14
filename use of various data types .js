076ed006003c0f44fc18d7ba892d46a458a26a31
Yash <yp421416@gmail.com>
2025-12-14 22:20:53 +0530

    Add example usage of various data types in JS

---
+++ use of various data types .js
@@ -0,0 +1,22 @@
+let arr = [1, 2, 3];
+console.log(`Value: ${arr} | Type: ${Array.isArray(arr) ? "array" : typeof arr}`);
+
+//object 
+let obj = { name: "Yash", age: 19 };
+console.log(`Value:`, obj, `| Type:`, typeof obj);
+
+//number
+let num = 10;
+console.log(`Value: ${num} | Type: ${typeof num}`);
+
+//string
+let str = "Yash";
+console.log(`Value: ${str} | Type: ${typeof str}`);
+
+//boolean
+let isFit = true;
+console.log(`Value: ${isFit} | Type: ${typeof isFit}`);
+
+//undefined
+let x;
+console.log(`Value: ${x} | Type: ${typeof x}`);
